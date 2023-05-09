c6eb8a01fa4ebb9fbebe453c69e00c9194eeb0af
Alvaise <79141448+Alvaise@users.noreply.github.com>
2023-05-09 10:40:48 +0200

    aa

---
+++ drim-tim/src/main/java/com/drim/DrimTimApplication.java
@@ -0,0 +1,13 @@
+package com.drim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrimTimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrimTimApplication.class, args);
+	}
+
+}
